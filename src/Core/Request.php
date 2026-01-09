6fd8120e0097c13cecfe3d5dc0ef83ab08687d9d
Phillip Harrington <pharrington@abre.com>
2026-01-08 20:31:44 -0500

    request query + composer update

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -39,6 +39,11 @@ class Request
         return $this->query;
     }
 
+    public function query(string $key, $default = null)
+    {
+        return $this->query[$key] ?? $default;
+    }
+
     public function getPostParams(): array
     {
         return $this->post;
