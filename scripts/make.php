1b462f023cb393312fe0f53fff0f1e6eb7b5ebf2
Phillip Harrington <pharrington@abre.com>
2026-02-03 15:15:19 -0500

    Adds table and record maker

--- scripts/make.php
+++ scripts/make.php
@@ -9,6 +9,10 @@
  *   php make.php make:scaffold GamePlay/CreateSeries
  *   php make.php make:scaffold GamePlay/CreateSeries --no-pipe
  *   php make.php make:scaffold Events/UserCreated --event-only
+ *   php make.php make:record User
+ *   php make.php make:record Auth/User --no-uuid
+ *   php make.php make:table Users
+ *   php make.php make:table Auth/Users --record=UserRecord
  *
  * Run without arguments to see available commands:
  *   php make.php
@@ -28,8 +32,10 @@ require_once $autoloadPath;
 use Handlr\Generator\GeneratorRunner;
 use Handlr\Generator\Makers\MakerMaker;
 use Handlr\Generator\Makers\MigrationMaker;
+use Handlr\Generator\Makers\RecordMaker;
 use Handlr\Generator\Makers\ScaffoldMaker;
 use Handlr\Generator\Makers\SeederMaker;
+use Handlr\Generator\Makers\TableMaker;
 
 $stubsPath = dirname(__DIR__) . '/stubs';
 
@@ -37,6 +43,8 @@ $runner = new GeneratorRunner($stubsPath);
 $runner
     ->register(new MakerMaker())
     ->register(new MigrationMaker())
+    ->register(new RecordMaker())
     ->register(new ScaffoldMaker())
     ->register(new SeederMaker())
+    ->register(new TableMaker())
     ->run();
